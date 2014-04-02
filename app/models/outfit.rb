c4e5d3d1a65e3c294161bff2c73946a1dab7c459
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-04-02 11:12:13 -0400

    Adding validations to outfits

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -8,7 +8,9 @@ class Outfit < ActiveRecord::Base
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
-
+  validates(:description,  { :presence => true } )
+  validates(:brand_name,  { :presence => true } )
+  validates(:style,  { :presence => true } )
 end
 
 # @outfit = Outfit.new(:description => "cool")
