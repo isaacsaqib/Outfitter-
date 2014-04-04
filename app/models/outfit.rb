36e0e90952a1c89b8e6cab46e44aedbc10b05d28
isaacsaqib <isaac.saqib@stonybrook.edu>
2014-04-04 09:52:26 -0400

    fixing picture not showing up in heroku issue

--- app/models/outfit.rb
+++ app/models/outfit.rb
@@ -4,8 +4,8 @@ class Outfit < ActiveRecord::Base
   has_many :votes
   belongs_to :user
   has_attached_file :photo, :styles => { :small => "300x450>" },
-                    :url  => "/:attachment/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/:attachment/:id/:style/:basename.:extension"
+                    :url  => "/assets/products/:id/:style/:basename.:extension",
+                    :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
